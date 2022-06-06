d3bf772061403136c5f3790dda44072e91951663
James Clark <hazywizardstudio@gmail.com>
2022-06-06 16:57:50 -0400

    init

---
+++ bray.js
@@ -0,0 +1,14 @@
+
+const burgerMenu = document.querySelector("#hambuger")
+
+function changePage(newPage){console.log(newPage);document.getElementById("bodyMain").innerHTML=`<object type="text/html" data=pages/${newPage}></object>`;document.getElementById("hamburgerMenu").style.display="none"}
+
+window.addEventListener("load", function(){
+    changePage("News.html");
+    }
+)
+
+function toggleMenu(){
+    document.getElementById("hamburgerMenu").style.display="flex";
+    console.log("hamburgerWasClicked")
+}
